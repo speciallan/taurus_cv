aa3f99ba527f73e2d0b523d03a86c46fc1c77fb2
speciallan <350394776@qq.com>
2019-05-18 02:54:24 +0800

    增加snet

---
+++ models/resnet/snet.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:Speciallan
+
+from keras import Model, Input
+from keras.layers import *
+
+def snet(input, classes_num=2, is_extractor=False):
+
+    x = Conv2D(filters=16, kernel_size=(3, 3), strides=2, activation='relu', padding='same', name='snet_conv1')(input)
+    x = Conv2D(16, (1, 1), strides=1, activation='relu', padding='same', name='snet_conv2')(x)
+    x = Conv2D(16, (1, 1), strides=1, activation='relu', padding='same', name='snet_conv3')(x)
+    x = Conv2D(32, (3, 3), strides=2, activation='relu', padding='same', name='snet_conv4')(x)
+    x = Conv2D(64, (3, 3), strides=1, activation='relu', padding='same', name='snet_conv5')(x)
+    x = Conv2D(128, (3, 3), strides=1, activation='relu', padding='same', name='snet_conv6')(x)
+    x = MaxPooling2D((2, 2), strides=1, name='snet_pool')(x)
+
+    if is_extractor:
+        return x
+
+    else:
+
+        # x = MaxPooling2D()(x)
+        x = Flatten()(x)
+        x = Dense(classes_num, activation='softmax', kernel_initializer='normal')(x)
+        model = Model(input, x)
+
+        return model
+
+
+
